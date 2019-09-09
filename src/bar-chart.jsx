3b7b5f207fabd4e6b035de84302af889bade8a21
Domonic Adams <domonicadams@Domonics-MacBook-Pro.local>
2019-09-09 11:40:37 -0700

    Small refactor

--- src/bar-chart.jsx
+++ src/bar-chart.jsx
@@ -21,7 +21,7 @@ class BarChart extends Component {
     const dataMax = max(data);
     const yScale = scaleLinear()
         .domain([0, dataMax])
-        .range([size[1], 0]);
+        .range([0, size[1]]);
     select(node)
       .selectAll('rect')
       .data(data)
@@ -33,23 +33,23 @@ class BarChart extends Component {
       .data(data)
       .exit()
       .remove();
-    
+
     select(node)
       .selectAll('rect')
       .data(data)
       .attr('x', (d,i) => i * 25)
-      .attr('y', d => yScale(0))
+      .attr('y', d => size[1] - yScale(0))
       .attr('height', d => size[1] - yScale(0))
       .attr('width', 25)
-      .attr("fill", function(d, i) { return colors[i % 3]});
+      .attr("fill", (d, i) => colors[i % 3]);
     
     select(node)
       .selectAll('rect')
       .transition()
       .duration(800)
-      .attr("y", function(d) { return  size[1] - yScale(d); })
-      .attr("height", function(d) { return  yScale(d); })
-      .delay(function(d,i){console.log(d) ; return(i*100)})
+      .attr("y", d => size[1] - yScale(d))
+      .attr("height", d => yScale(d))
+      .delay((_,i) => i*100)
   }
 
   render() {
